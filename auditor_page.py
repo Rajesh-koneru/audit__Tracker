fb85a902ff7d94f3dfec9419a2bc8faf540eec35
Raju <rajeshkoneru1820@gmail.com>
2025-01-09 16:04:22 +0530

    Add files via upload

--- auditor_page.py
+++ auditor_page.py
@@ -1,6 +1,9 @@
 from flask import Flask , render_template , url_for , redirect,request,abort, Blueprint
+from flask_login import login_required
+from test import only_one
 hello=Blueprint(__name__ ,'auditor')
 @hello.route('/auditor')
+@login_required
+@only_one('admin','auditor')
 def auditor_page():
     return 'hello auditor'
-auditor_page()
